1f60178103e0b1bd23c6822c06fe6eb79168f0c0
Nitin Bansal <bansalnitin92@gmail.com>
2020-03-26 15:34:32 +0530

    first commit

---
+++ src/main/java/com/nitin/mtest/MTestApplication.java
@@ -0,0 +1,13 @@
+package com.nitin.mtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitin/mtest/MTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitin.mtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
